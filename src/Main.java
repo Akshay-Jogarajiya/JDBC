1ad4f413d34f1241b1d512934ea1b0f37c001977
Akshay-Jogarajiya <jogarajiyaakshay@gmail.com>
2025-05-24 22:28:59 +0530

    perform insert query

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,38 @@ public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/jdbc";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
+    static Connection connection = null;
 
     public static void main(String[] args) {
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            insertStudent();
+            retrieveStudent();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static void insertStudent() {
+        try {
+            Statement stmt = connection.createStatement();
+            String query = String.format("INSERT INTO STUDENTS(name, age, marks) VALUES('%s', %o, %f)", "Paresh", 20, 95.55);
+            int rowsAffected = stmt.executeUpdate(query);
+
+            if (rowsAffected > 0) {
+                System.out.println("Student inserted successfully");
+            }
+            else {
+                System.out.println("Student insert failed");
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    static void retrieveStudent() {
+        try {
             Statement statement = connection.createStatement();
             String query = "SELECT * FROM STUDENTS";
             ResultSet resultSet = statement.executeQuery(query);
@@ -23,6 +49,7 @@ public class Main {
                 System.out.println("Name :" +resultSet.getString("name"));
                 System.out.println("age :" +resultSet.getInt("age"));
                 System.out.println("marks :" +resultSet.getDouble("marks"));
+                System.out.println("-----------------------------------------------------");
             }
         }
         catch (Exception e) {
